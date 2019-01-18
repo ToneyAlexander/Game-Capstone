6529506ccb4b1d2d6d3ff34c3b031ae187da31d6
Maple Spyder <jett.30@osu.edu>
2019-01-18 17:13:02 -0500

    ICommand, AbstractCommand

---
+++ Assets/Scripts/Commands/AbstractCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractCommand<TReceiver> : ICommand
+{
+    private TReceiver receiver;
+    protected AbstractCommand(TReceiver receiver)
+    {
+        this.receiver = receiver;
+    }
+
+    protected TReceiver Receiver
+    {
+        get { return receiver; }
+    }
+
+    public abstract void InvokeCommand();
+}
