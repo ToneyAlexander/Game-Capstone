c609a11f614fd517454468bff1a699742df98431
Yegie <yegres.vestlam@gmail.com>
2019-01-18 17:59:25 -0500

    physics
    ball bounces around and gains var speed on bounce.

---
+++ Assets/Pong Stuff/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallController : MonoBehaviour
+{
+    const float basespeed = 7f;
+    const float speedGain = 1.3f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-1f, 1f)) * basespeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        rb.velocity = rb.velocity * speedGain;
+    }
+   
+}
