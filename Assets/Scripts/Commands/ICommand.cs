346b3d6cee1e6af8c27136171fa9640e75e5ae02
Wesley Brown <nipponkid7@gmail.com>
2019-01-19 00:07:46 -0500

    Implement Movement Based on Commands
    Implement the movement of GameObjects based on the Command design pattern and
    create an InputDetector singleton as a ScriptableObject. This will allow the
    movement of Movables to be independent of all input.

--- Assets/Scripts/Commands/ICommand.cs
+++ Assets/Scripts/Commands/ICommand.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICommand
+﻿public interface ICommand
 {
     void InvokeCommand();
 }
---
+++ Assets/Scripts/InputDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Detects input.
+/// </summary>
+public class InputDetector : MonoBehaviour
+{
+    [SerializeField]
+    private CommandProcessor commandProcessor;
+
+    [SerializeField]
+    private Movable movable;
+
+    private void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(0.0f, 0.0f, z);
+        commandProcessor.ProcessCommand(new MoveCommand(movable, velocity));
+    }
+}
