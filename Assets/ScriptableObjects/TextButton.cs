cf3204348ba27ed5fd54b9a2d63421fb33296c6f
nimmagadda.8 <nimmagadda.8@buckeyemail.osu.edu>
2019-01-21 13:35:55 -0500

    Added basic pause menu

---
+++ Assets/ScriptableObjects/TextButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextButton : MonoBehaviour
+{
+    [SerializeField]
+    private string textField = "Run";
+
+    public ICommand command;
+    private Text text;
+
+    void Activate(GameObject canvasObject)
+    {
+        transform.parent = canvasObject.transform;
+        text = gameObject.AddComponent<Text>();
+        text.text = textField;
+
+    }
+    void highlight()
+    {
+
+    }
+    
+}
