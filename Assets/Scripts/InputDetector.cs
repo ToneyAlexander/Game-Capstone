3d21026d939421dc9bb77679322c168d40c28be1
Wesley Brown <nipponkid7@gmail.com>
2019-01-21 14:02:24 -0500

    Change InputManager to be a ScriptableObject
    Change the InputManager script to be a ScriptableObject instead of a
    MonoBehaviour. Since there should only ever be one instance of InputManager, it
    is a singleton and therefore makes sense to be a ScriptableObject. Also, this
    will allow it to be referenced in the Unity Editor. Any Component that needs
    to access the InputManager singleton just has to have a reference to the
    PlayerInputManager ScriptableObject asset dragged to one of its fields and
    Unity will take care of connecting them. This removes the need to do this in
    code and provides visual debugging.

--- Assets/Scripts/InputDetector.cs
+++ Assets/Scripts/InputDetector.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 /// <summary>
-/// Detects input.
+/// Causes a GameObject to detect input from an input source. Requires a reference to a CommandProcessor.
 /// </summary>
+[RequireComponent(typeof(Movable))]
 public class InputDetector : MonoBehaviour
 {
     [SerializeField]
-    private CommandProcessor commandProcessor;
+    private InputManager inputManager;
 
-    [SerializeField]
     private Movable movable;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(inputManager, name + "'s InputManager is null!");
+    }
+
+    private void Start()
+    {
+        movable = GetComponent<Movable>();
+    }
 
     private void Update()
     {
-        float z = Input.GetAxis("Vertical");
-        Vector3 velocity = new Vector3(0.0f, 0.0f, z);
-        commandProcessor.ProcessCommand(new MoveCommand(movable, velocity));
+        float horizontalAxisValue = Input.GetAxis("Horizontal");
+        inputManager.HandleHorizontalAxisInput(horizontalAxisValue, movable);
+
+        float verticalAxisValue = Input.GetAxis("Vertical");
+        inputManager.HandleVerticalAxisInput(verticalAxisValue, movable);
     }
 }
