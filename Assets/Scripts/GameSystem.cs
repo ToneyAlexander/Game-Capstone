a3420ae0337efc623946db033ca43c3bd9ecd68e
nimmagadda.8 <nimmagadda.8@buckeyemail.osu.edu>
2019-01-21 14:46:04 -0500

    Added pause command

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem
+{
+    static bool isPaused = false;
+    public static bool getPaused()
+    {
+        return isPaused;
+    }
+    public static void togglePaused()
+    {
+        isPaused = !isPaused;
+    }
+    public static void setPaused(bool pause)
+    {
+        isPaused = pause;
+    }
+}
