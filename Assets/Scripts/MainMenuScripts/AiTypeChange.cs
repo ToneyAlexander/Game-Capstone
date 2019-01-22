5c7f6c03389ef7e3da18ad240e204a8e381b52fa
Haoran Wang <howiehowiewow@gmail.com>
2019-01-22 00:09:21 -0500

    finishing main menu

---
+++ Assets/Scripts/MainMenuScripts/AiTypeChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AiTypeChange : MonoBehaviour {
+
+    public Text text;
+    public void ChangeText()
+    {
+        if(text.text == "Stupid"){
+            text.text = "Lazy";
+        }
+
+        else if(text.text == "Lazy"){
+            text.text = "Smart";
+        }
+        else if (text.text == "Smart")
+        {
+            text.text = "Stupid";
+        }
+    }
+}
