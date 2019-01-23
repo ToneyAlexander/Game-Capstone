81678774db1d5f8e9ffc621cf53c35e7f87881dc
SquiddyStardust <thecanvashat@gmail.com>
2019-01-23 17:01:46 -0500

    added menu functionality to the pause menu

---
+++ Assets/Scripts/Commands/MainMenuCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuCommand : ICommand
+{
+
+    public void InvokeCommand()
+    {
+        GameSystem.setPaused(false);
+        GameSystem.resetMenu();
+        SceneManager.LoadScene("Scenes/Menu");
+       
+    }
+}
