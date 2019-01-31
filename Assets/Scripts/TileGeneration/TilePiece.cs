7d5fceb372c4bcf12e47df153d03905f384f16e9
Maplespyder <mclarenf1lm22@gmail.com>
2019-01-31 00:32:08 -0500

    added tile script (going to clean the code still) and whitebox tiles

---
+++ Assets/Scripts/TileGeneration/TilePiece.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Class for tilePieces to be laid
+ */
+public class TilePiece
+{
+
+    public GameObject prefab;
+    public string colorID;
+    public int rotation;
+    public Vector3 modif;
+
+    public TilePiece(GameObject _prefab, string _colorID, int _rotiation, Vector3 _modif)
+    {
+        prefab = _prefab;
+        colorID = _colorID;
+        rotation = _rotiation;
+        modif = _modif;
+    }
+}
