bfcf1f3525a5db9b13e01cc6d1ceb497ef333478
Haoran Wang <howiehowiewow@gmail.com>
2019-02-06 18:39:32 -0500

    Remy is using gravity and will not stop by no reason on moving. but doesn't stop when collision with walls right the way

--- Assets/Scripts/CharacterMovement/RemyWithWall.cs
+++ Assets/Scripts/CharacterMovement/RemyWithWall.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class RemyWithWall : MonoBehaviour
 {
 
-    private Collision col;
     private void OnCollisionEnter(Collision collision)
-    {
-        col = collision;
-        
+    {   
         if (collision.gameObject.CompareTag("Wall"))
         {
             //Debug.Log("撞墙");
 
             //transform.position = RemyController.lastPosition;
 
-            RemyController.destination = transform.position;
+            //RemyController.destination = transform.position;
 
         }
     }
