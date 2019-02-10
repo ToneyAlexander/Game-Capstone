85d820c1c6c78e1ca974c75a7ef713fc4b9f2e6d
Maplespyder <mclarenf1lm22@gmail.com>
2019-02-10 03:09:50 -0500

    added spawn chance for treeSpawn

--- Assets/Tiles/EnvironmentGenerator.cs
+++ Assets/Tiles/EnvironmentGenerator.cs
@@ -16,11 +16,18 @@ public class EnvironmentGenerator : MonoBehaviour
     private void replaceObjectsToTrees()
     {
         GameObject[] listOfTrees = GameObject.FindGameObjectsWithTag("Tree");
+        //a tree's spawn chance shall be 75%
         System.Random rnd = new System.Random();
+        rnd = new System.Random();
+        System.Random rnd2 = new System.Random();
         foreach (GameObject x in listOfTrees)
         {
             int index = rnd.Next(0, treeList.EnvironmentList.Count);
-            GameObject newObject = Instantiate(treeList.EnvironmentList[index], x.transform.position + treeList.EnvironmentList[index].transform.position, Quaternion.identity);
+            int spawnRoll = rnd2.Next(0, 100);
+            if (spawnRoll <= 75) //75% chance
+            {
+                GameObject newObject = Instantiate(treeList.EnvironmentList[index], x.transform.position + treeList.EnvironmentList[index].transform.position, Quaternion.identity);
+            }
             Destroy(x);
         }
     }
