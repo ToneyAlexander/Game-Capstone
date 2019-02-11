0ad126c2ce75fd259b9c1a28a7cfc73005aa692b
Maplespyder <mclarenf1lm22@gmail.com>
2019-02-10 23:45:31 -0500

    changed generation code; added grass / bushrock stuff

--- Assets/Tiles/EnvironmentGenerator.cs
+++ Assets/Tiles/EnvironmentGenerator.cs
@@ -6,27 +6,34 @@ public class EnvironmentGenerator : MonoBehaviour
 {
     [SerializeField]
     private EnvironmentData treeList;
+    [SerializeField]
+    private EnvironmentData grassList;
+    [SerializeField]
+    private EnvironmentData mediumObjectList;
 
     // Start is called before the first frame update
     void Start()
     {
-        replaceObjectsToTrees();
+        replaceObjects(75, treeList.EnvironmentList, "Tree");
+        replaceObjects(90, grassList.EnvironmentList, "Grass");
+        replaceObjects(90, mediumObjectList.EnvironmentList, "MediumObject");
     }
 
-    private void replaceObjectsToTrees()
+    private void replaceObjects(int percentage, List<GameObject> environmentList, string type)
     {
-        GameObject[] listOfTrees = GameObject.FindGameObjectsWithTag("Tree");
+        GameObject[] listOfObjects = GameObject.FindGameObjectsWithTag(type);
         //a tree's spawn chance shall be 75%
         System.Random rnd = new System.Random();
         rnd = new System.Random();
         System.Random rnd2 = new System.Random();
-        foreach (GameObject x in listOfTrees)
+        foreach (GameObject x in listOfObjects)
         {
-            int index = rnd.Next(0, treeList.EnvironmentList.Count);
+            int index = rnd.Next(0, environmentList.Count);
             int spawnRoll = rnd2.Next(0, 100);
-            if (spawnRoll <= 75) //75% chance
+            if (spawnRoll <= percentage) //75% chance
             {
-                GameObject newObject = Instantiate(treeList.EnvironmentList[index], x.transform.position + treeList.EnvironmentList[index].transform.position, Quaternion.identity);
+                GameObject newObject = Instantiate(environmentList[index], x.transform.position + environmentList[index].transform.position, Quaternion.identity);
+                newObject.transform.rotation = new Quaternion(0, Random.rotation.y, 0, 1);
             }
             Destroy(x);
         }
