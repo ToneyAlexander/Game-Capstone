fd0a730f6a562f11c356ab9cbf76c927d58aceca
SquiddyStardust <thecanvashat@gmail.com>
2019-02-13 10:23:15 -0500

    Camera rotation on Q and E. Able to change camera speed independent of player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,12 +11,40 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Vector3 cameraPosition;
 
+    private Vector3[] cameras;
+
+    int direction = 0;
+
+    void Start()
+    {
+        cameras = new Vector3[4];
+        float angle = 0;
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            float sin = Mathf.Sin(angle);
+            float cos = Mathf.Cos(angle);
+            cameras[i] = new Vector3(cameraPosition.x * cos - cameraPosition.z * sin, cameraPosition.y, cameraPosition.x * sin - cameraPosition.z * cos);
+            Debug.Log(cameras[i]);
+            angle += Mathf.PI * 2 / cameras.Length;
+        }
+    }
+
     // Update is called once per frame
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            direction = (direction + 1) % cameras.Length;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            direction = (direction + 1) % cameras.Length;
+        }
         Vector3 position = player.transform.position;
-        position += cameraPosition;
-        transform.position = position;
-        
+        position += cameras[direction];
+        transform.position = Vector3.MoveTowards(transform.position, position, 20 * Time.deltaTime) ;
+        transform.LookAt(player.transform);
+
     }
 }
