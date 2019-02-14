3beef4a554eea34f5eb323bd4834b97ee6b4db31
Wesley Brown <nipponkid7@gmail.com>
2019-02-14 14:52:03 -0500

    Add Basic Implementation of System for Ability Data
    Add the basic foundation for the system that allows the data of abilities to
    be defined in the Unity inspector. This will allow the actual abilities to be
    separate from the ability data itself.

---
+++ Assets/ScriptableObjectScripts/Abilities/Ability.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using CCC.Stats;
+
+namespace CCC.Abilities
+{
+    [CreateAssetMenu]
+    public sealed class Ability : ScriptableObject
+    {
+        public string AbilityName
+        {
+            get { return abilityName; }
+        }
+
+        public Sprite Icon
+        {
+            get { return icon; }
+        }
+
+        public GameObject Prefab
+        {
+            get { return prefab; }
+        }
+
+        public List<Stat> Stats
+        {
+            get { return stats; }
+        }
+
+        /// <summary>
+        /// The name of this Ability.
+        /// </summary>
+        [SerializeField]
+        private string abilityName;
+
+        [SerializeField]
+        private Sprite icon;
+
+        /// <summary>
+        /// The prefab that represents the animation of this Ability.
+        /// </summary>
+        [SerializeField]
+        private GameObject prefab;
+
+        [SerializeField]
+        private List<AbilityStatSlotEntry> statSlots;
+
+        private List<Stat> stats;
+
+        #region ScriptableObject Messages
+        private void OnEnable()
+        {
+            stats = new List<Stat>();
+            foreach (AbilityStatSlotEntry entry in statSlots)
+            {
+                Stat stat = new Stat(entry.StatName, entry.Value);
+                stats.Add(stat);
+            }
+        }
+        #endregion
+    }
+
+    [System.Serializable]
+    class AbilityStatSlotEntry
+    {
+        public string StatName
+        {
+            get { return statIdentifier.InternalStatName; }
+        }
+
+        public float Value
+        {
+            get { return value; }
+        }
+
+        [SerializeField]
+        private StatIdentifier statIdentifier;
+
+        [SerializeField]
+        private float value;
+    }
+}
---
+++ Assets/ScriptableObjectScripts/Abilities/AbilityStatSlotEntry.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using CCC.Stats;
+
+namespace CCC.Abilities
+{
+    [System.Serializable]
+    public struct AbilityStatSlotEntry1
+    {
+        public string StatName
+        {
+            get { return statIdentifier.InternalStatName; }
+        }
+
+        public float Value
+        {
+            get { return value; }
+        }
+
+        [SerializeField]
+        private StatIdentifier statIdentifier;
+
+        [SerializeField]
+        private float value;
+    }
+}
