873fbd255b77b82db6fed217912fef26dffa278f
Wesley Brown <nipponkid7@gmail.com>
2019-02-14 23:04:51 -0500

    Create the Initial Implementation of the AbilityUser Component
    The AbilityUser Component will allow a GameObject to use a Ability.

---
+++ Assets/Scripts/Abilities/AbilityUser.cs
@@ -0,0 +1,33 @@
+﻿using CCC.Abilities;
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Represents a Component that allows a GameObject to use an Ability.
+/// </summary>
+public sealed class AbilityUser : MonoBehaviour
+{
+    /// <summary>
+    /// The Abilities that this GameObject can use.
+    /// </summary>
+    [SerializeField]
+    private List<Ability> usableAbilities;
+
+    /// <summary>
+    /// Use an Ability.
+    /// </summary>
+    /// <param name="ability">The Ability to use.</param>
+    public void Use(Ability ability)
+    {
+        Debug.Log(gameObject.name + " used Ability " + ability.AbilityName);
+    }
+
+    private void Start()
+    {
+        // Only for testing
+        foreach (Ability ability in usableAbilities)
+        {
+            Use(ability);
+        }
+    }
+}
