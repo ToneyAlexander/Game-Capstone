278d5da97c8d38c44665eb3488c7edb2956edc1c
SquiddyStardust <thecanvashat@gmail.com>
2019-02-15 17:57:08 -0500

    stat screen is now functional

--- Assets/StatScreenUI.cs
+++ Assets/StatScreenUI.cs
@@ -9,13 +9,26 @@ public class StatScreenUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log("kurai MY LIFE");
+        if (gameObjects.Length > 0)
+        {
+            Debug.Log("kurai MY SIGHT");
+            ControlStatBlock temp = gameObjects[0].GetComponent<ControlStatBlock>();
+            if (temp)
+            {
+                Debug.Log("kurai MY HEART IN SUCH UNCOMMON PLACES");
+                stats = temp;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Text statlist = gameObject.GetComponent<Text>();
+        Text statlist = gameObject.GetComponentInChildren<Text>();
+        //Debug.Log(statlist);
+        
         statlist.text = "Strength: " + stats.Str + "\n\nDexterity: " + stats.Dex + "\n\nMysticism: " + stats.Myst + "\n\nFortitude: " + stats.Fort;
 
     }
