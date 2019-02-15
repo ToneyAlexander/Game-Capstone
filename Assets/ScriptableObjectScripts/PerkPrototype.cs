cf777c8be204dec9c616fa710000e4ee7a9554a3
Yegie <yegres.vestlam@gmail.com>
2019-02-15 18:25:37 -0500

    Fixed arsonist missing stats and propogated children

--- Assets/ScriptableObjectScripts/PerkPrototype.cs
+++ Assets/ScriptableObjectScripts/PerkPrototype.cs
@@ -19,8 +19,16 @@ public class PerkPrototype : ScriptableObject
     public Vector2 uiCoords;
     public Sprite sprite;
 
+    public void Awake()
+    {
+        Debug.Log("awake");
+        Children.Clear();
+        Blocks.Clear();
+    }
+
     void OnEnable()
     {
+        Debug.Log("enable");
         foreach (PerkPrototype p in Require)
         {
             p.Children.Add(this);
