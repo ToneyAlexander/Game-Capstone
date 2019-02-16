66590f1a3cc8936047e3903d85c59ce1d2d0b7aa
SquiddyStardust <thecanvashat@gmail.com>
2019-02-15 19:07:17 -0500

    fixed InventoryTest

--- Assets/InfoMenuScript.cs
+++ Assets/InfoMenuScript.cs
@@ -11,6 +11,7 @@ public class InfoMenuScript : MonoBehaviour
     GameObject stats;
     GameObject classinfo;
     // Start is called before the first frame update
+
     void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
