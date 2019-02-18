2797b171833855a6f8ceecdeb3a16d76179941ad
SquiddyStardust <thecanvashat@gmail.com>
2019-02-18 18:35:40 -0500

    added tab button script for M I C R O I N T E R A C T I O N

---
+++ Assets/Scripts/UIScripts/TabButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour
+{
+    Vector3 selectedPos;
+    Vector3 hiddenPos;
+    Color selectedColor = Color.red;
+    Color hiddenColor = new Color(29, 194, 202);
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedPos = new Vector3(transform.localPosition.x, 175, transform.localPosition.z);
+        
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+       Image col = gameObject.GetComponent<Image>();
+        /*
+        timer += Time.deltaTime;
+        timer = Mathf.Min(timer, 1);
+        col.color = Color.Lerp(hiddenColor, selectedColor, timer);
+        */
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, selectedPos, 80 * Time.deltaTime);
+       // Debug.Log(timer);
+    }
+}
