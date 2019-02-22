3dc055b789793aff9485d9d650c67f489fd45eff
SquiddyStardust <thecanvashat@gmail.com>
2019-02-22 17:46:22 -0500

    working on boats

---
+++ Assets/BoatCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatCameraController : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        transform.RotateAround(Vector3.zero, Vector3.up, h*Time.deltaTime);
+
+
+    }
+}
