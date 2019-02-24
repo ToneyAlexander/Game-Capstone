e7b5012db95a9f8ebdfedeaba6dcdb5fcbc0de68
Maplespyder <mclarenf1lm22@gmail.com>
2019-02-23 20:12:56 -0500

    basic nonfunctioning HUD: Hotbar, EXPBar and HP/MP bars.

---
+++ Assets/HUD/ExperienceBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private float currentExperience;
+
+    private float endExperience;
+
+    private int currentLevel;
+
+    private PlayerClass player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentExperience >= endExperience)
+        {
+        }
+    }
+
+    void experienceGain(float experienceGained)
+    {
+        
+    }
+}
