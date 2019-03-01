405781e5d9eed65b1cfecb482030480bca8763a5
ToneyAlexander <Alex.Toney11@gmail.com>
2019-03-01 18:01:08 -0500

    Added island templates, randomly picked. Remy starts on guarenteed beach, started on heuristics

--- Assets/Scripts/TileGeneration/TilePiece.cs
+++ Assets/Scripts/TileGeneration/TilePiece.cs
@@ -13,12 +13,24 @@ public class TilePiece
     public int ID;
     public int rotation;
     public Vector3 modifier;
+    public bool[] navigability;
 
-    public TilePiece(GameObject _prefab, int _ID, int _rotiation, Vector3 _modifier)
+    public TilePiece(GameObject _prefab, int _ID, int _rotiation, Vector3 _modifier, bool top, bool right, bool bottom, bool left)
     {
         prefab = _prefab;
         ID = _ID;
         rotation = _rotiation;
-        modifier = _modifier;  
+        modifier = _modifier;
+        bool[] nav = { top, right, bottom, left };
+        navigability = nav;
+    }
+
+    public TilePiece(GameObject _prefab, int _ID, int _rotiation, Vector3 _modifier, bool[] _navigability)
+    {
+        prefab = _prefab;
+        ID = _ID;
+        rotation = _rotiation;
+        modifier = _modifier;
+        navigability = _navigability;
     }
 }
