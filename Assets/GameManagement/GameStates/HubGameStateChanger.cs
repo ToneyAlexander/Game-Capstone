2982b15bd232492a9d447f32aead8486135e471e
SquiddyStardust <thecanvashat@gmail.com>
2019-03-04 18:16:12 -0500

    added hub scene transitioner

---
+++ Assets/GameManagement/GameStates/HubGameStateChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CCC.GameManagement.GameStates;
+using CCC.GameManagement;
+
+public class HubGameStateChanger : MonoBehaviour, IGameStateChanger
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private SceneChanger sceneChanger;
+
+    [SerializeField]
+    private Game game;
+    [SerializeField]
+    private SceneReference sceneReference;
+
+
+    public void ChangeGameState()
+    {
+        game.CurrentState = new PlayIslandGameState(sceneChanger,
+            sceneReference);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        sceneChanger = GetComponent<SceneChanger>();
+    }
+}
