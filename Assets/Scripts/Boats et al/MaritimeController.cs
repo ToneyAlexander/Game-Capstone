53542224bcf2ebbe8c8a207b84bf324e22e9f2fa
SquiddyStardust <thecanvashat@gmail.com>
2019-03-04 16:16:03 -0500

    boat scene fun with fixes

--- Assets/Scripts/Boats et al/MaritimeController.cs
+++ Assets/Scripts/Boats et al/MaritimeController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MaritimeController : MonoBehaviour
 {
 
-    private struct Island
+    public struct Island
     {
         public Vector3 position;
         public string name;
@@ -23,6 +23,7 @@ public class MaritimeController : MonoBehaviour
     public GameObject player;
     public GameObject prefab;
     public ThemeDictionary themes;
+    public NameGenerator nameGen;
 
     //  public List<Vector3> islandpos = new List<>
     // Start is called before the first frame update\
@@ -30,17 +31,24 @@ public class MaritimeController : MonoBehaviour
     int minnum = 6;
     int nums;
     float tolerance = 15.0f;
-    List<Island> islands = new List<Island>();
+    public List<Island> islands = new List<Island>();
 
     void Start()
     {
-       int nums = (int)Mathf.Floor(Random.Range(minnum, maxnum));
+       float nums = Mathf.Floor(Random.Range(minnum, maxnum));
         for (int i = 0; i < nums; i++)
         {
             float cos = Mathf.Cos(i / nums * 2 * Mathf.PI);
             float sin = Mathf.Sin(i / nums * 2 * Mathf.PI);
-           GameObject x =  Instantiate(prefab, new Vector3(sin*200, 0, cos*200),Quaternion.identity);
-            Island isle = new Island(1, new Vector3(sin * 200, 0, cos * 200),"WARG",x);
+            GameObject x =  Instantiate(prefab, new Vector3(sin*200, 0, cos*200),Quaternion.identity);
+            
+            x.GetComponent<positionText>().player = player;
+            //  x
+            string name = nameGen.generateName();
+            Debug.Log(name);
+            x.GetComponent<positionText>().setName(name);
+            
+            Island isle = new Island(1, new Vector3(sin * 200, 0, cos * 200),name,x);
         }
 
     }
