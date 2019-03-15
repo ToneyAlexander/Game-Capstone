409f4487a3f22c033adf9635430d72c60cfc108d
Haoran Wang <howiehowiewow@gmail.com>
2019-03-14 20:56:24 -0500

    Remy died sound added

---
+++ Assets/Scripts/SoundEffect/Died.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Died: MonoBehaviour
+{
+    private AudioSource audiodata;
+    public AudioClip DiedClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiodata = GetComponent<AudioSource>();
+    }
+
+    void PlayDied()
+    {
+        audiodata.clip = DiedClip;
+        audiodata.Play();
+    }
+}
