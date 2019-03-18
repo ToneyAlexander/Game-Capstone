4c6edb550a008f201eb1e0befde8cadafa58781d
Maple Spyder <jett.30@osu.edu>
2019-03-18 11:47:31 -0400

    fixed HUD positioning & functioning EXP Bar

--- Assets/HUD/ExperienceBar.cs
+++ Assets/HUD/ExperienceBar.cs
@@ -1,33 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ExperienceBar : MonoBehaviour
 {
-
-    private float currentExperience;
-
-    private float endExperience;
-
-    private int currentLevel;
-
     private PlayerClass player;
+
+    private Image expBar;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+		if (gameObjects.Length > 0)
+		{
+			player = gameObjects[0].GetComponent<PlayerClass>();
+		}
+		expBar = this.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentExperience >= endExperience)
+        if(player.Exp >= player.ExpToLevel)
         {
-        }
-    }
-
-    void experienceGain(float experienceGained)
-    {
-        
+			expBar.fillAmount = 0;
+		}
+		else
+		{
+			expBar.fillAmount = player.Exp / player.ExpToLevel;
+		}
     }
 }
