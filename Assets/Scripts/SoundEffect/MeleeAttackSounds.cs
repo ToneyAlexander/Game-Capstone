046e740389d6495ebf383ef2a67584372c0c47c1
Haoran Wang <howiehowiewow@gmail.com>
2019-03-20 00:14:27 -0400

    Melee A sound added

---
+++ Assets/Scripts/SoundEffect/MeleeAttackSounds.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MeleeAttackSounds : MonoBehaviour
+{
+    private AudioSource audiodata;
+
+    [SerializeField]
+    private AudioClip MeleeA1;
+
+    [SerializeField]
+    private AudioClip MeleeA2;
+
+    [SerializeField]
+    private AudioClip MeleeA3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiodata = GetComponent<AudioSource>();
+    }
+
+    void PlayMeleeA1()
+    {
+        audiodata.clip = MeleeA1;
+        audiodata.Play();
+    }
+
+    void PlayMeleeA2()
+    {
+        audiodata.clip = MeleeA2;
+        audiodata.Play();
+    }
+
+    void PlayMeleeA3()
+    {
+        audiodata.clip = MeleeA3;
+        audiodata.Play();
+    }
+
+
+}
