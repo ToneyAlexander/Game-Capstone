6e6fc82a5938741fb770c74092f7ca45367c744a
SquiddyStardust <thecanvashat@gmail.com>
2019-03-21 23:42:30 -0400

    added basics of the crew system

---
+++ Assets/ScriptableObjectScripts/Crew/CrewMember.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewMember : ScriptableObject
+{
+    // Start is called before the first frame update
+    enum crewType
+    {
+
+    }
+    enum crewAttribute
+    {
+        Daring,
+        Wise,
+        Playful,
+        Suave,
+        Sophisticated,
+        Clever
+    }
+    [SerializeField]
+    private Sprite image;
+
+    [SerializeField]
+    private Color tint;
+
+    [SerializeField]
+    private int starRating;
+    public int StarRating{get{ return starRating; }}
+
+    [SerializeField]
+    private string _name;
+    public string name { get { return _name; } }
+
+    public void grantBonus()
+    {
+
+    }
+
+    
+}
