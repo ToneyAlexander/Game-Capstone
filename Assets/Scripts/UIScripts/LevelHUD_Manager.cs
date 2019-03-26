613cd6d2b0fb8e8dfa2276acc54342892d05c9fa
Maple Spyder <jett.30@osu.edu>
2019-03-26 13:31:26 -0400

    removed enemy packs from forest & added level on the HUD

---
+++ Assets/Scripts/UIScripts/LevelHUD_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelHUD_Manager : MonoBehaviour
+{
+	private PlayerClass player;
+
+	private Text textField;
+	// Start is called before the first frame update
+	void Start()
+    {
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+		if (gameObjects.Length > 0)
+		{
+			player = gameObjects[0].GetComponent<PlayerClass>();
+		}
+		textField = this.transform.GetComponent<Text>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		LevelUpdater();
+    }
+
+	void LevelUpdater()
+	{
+		textField.text = player.Level.ToString();
+	}
+}
