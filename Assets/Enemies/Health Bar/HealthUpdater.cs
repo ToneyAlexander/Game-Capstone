939bff94d3cdabbc1782aac78072a8b16f2d8efd
Un-Jarvis <huang.2089@osu.edu>
2019-03-30 22:28:46 -0400

    Updated health bar

---
+++ Assets/Enemies/Health Bar/HealthUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUpdater : MonoBehaviour
+{
+    private StatBlock stats;
+    private SimpleHealthBar healthBar;
+
+    void Awake()
+    {
+        stats = gameObject.transform.parent.transform.parent.GetComponentInParent<StatBlock>();
+        healthBar = GetComponent<SimpleHealthBar>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("max = " + stats.HealthMax);
+            Debug.Log("cur = " + stats.HealthCur);
+            Debug.Log("bas = " + stats.HealthBase);
+        }
+
+        healthBar.UpdateBar(stats.HealthBase, stats.HealthMax);
+    }
+}
