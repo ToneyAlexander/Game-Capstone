eddebb1d421f2a979e5ca8e72136c2a5799336c4
Maplespyder <mclarenf1lm22@gmail.com>
2019-03-31 20:16:54 -0400

    character info on stats page

--- Assets/Scripts/UIScripts/StatScreenUI.cs
+++ Assets/Scripts/UIScripts/StatScreenUI.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 public class StatScreenUI : MonoBehaviour
 {
     public ControlStatBlock stats;
-    Text statlist;
-    Text column1;
-    Text column2;
-    Text column3;
+    private Text statlist;
+    private Text column1;
+    private Text column2;
+    private Text column3;
+
+    private Text levelText; 
+
     private Text staticMainStats;
+
+    private Text classText;
+
+    private Text nameText;
+
+    private PlayerClass player;
+
+    private TestPlayerClass textPlayerClass;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,10 +49,27 @@ public class StatScreenUI : MonoBehaviour
                 column3 = transform.GetChild(j).gameObject.GetComponent<Text>();
 
             }
+            else if (transform.GetChild(j).name.Equals("LevelText"))
+            {
+                levelText = transform.GetChild(j).gameObject.GetComponent<Text>();
+
+            }
+            else if (transform.GetChild(j).name.Equals("ClassText"))
+            {
+                classText = transform.GetChild(j).gameObject.GetComponent<Text>();
+
+            }
+            else if (transform.GetChild(j).name.Equals("NameText"))
+            {
+                nameText = transform.GetChild(j).gameObject.GetComponent<Text>();
+
+            }
         }
         GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
         if (gameObjects.Length > 0)
         {
+            player = gameObjects[0].GetComponent<PlayerClass>();
+            textPlayerClass = gameObjects[0].GetComponent<TestPlayerClass>();
             ControlStatBlock temp = gameObjects[0].GetComponent<ControlStatBlock>();
             if (temp)
             {
@@ -62,5 +91,8 @@ public class StatScreenUI : MonoBehaviour
         column1.text = detailedStats.HealthMax + "\n" + StatBlock.CalcMult(detailedStats.HealthRegen, detailedStats.HealthRegenMult) + "\n" + StatBlock.CalcMult(detailedStats.Armor, detailedStats.ArmorMult) + "\n" + StatBlock.CalcMult(detailedStats.MoveSpeed, detailedStats.MoveSpeedMult) + "\n" + StatBlock.CalcMult(detailedStats.AttackSpeed, detailedStats.AttackSpeedMult);
         column2.text = StatBlock.CalcMult(detailedStats.Cdr,detailedStats.CdrMult) + "\n" + StatBlock.CalcMult(detailedStats.Spell, detailedStats.SpellMult) + "\n" + StatBlock.CalcMult(detailedStats.MagicRes, detailedStats.MagicResMult) + "\n" + StatBlock.CalcMult(detailedStats.StatusRec, detailedStats.StatusRecMult) + "\n" + StatBlock.CalcMult(detailedStats.AfflictRes, detailedStats.AfflictResMult);
         column3.text = StatBlock.CalcMult(detailedStats.RangedAttack, detailedStats.RangedAttackMult) + "\n" + StatBlock.CalcMult(detailedStats.MeleeAttack, detailedStats.MeleeAttackMult) + "\n" + StatBlock.CalcMult(detailedStats.Damage, detailedStats.DamageMult) + "\n" + StatBlock.CalcMult(detailedStats.CritDamage,detailedStats.CritDamageMult) + "\n" + StatBlock.CalcMult(detailedStats.CritChance,detailedStats.CritChanceMult);
+        levelText.text = player.Level.ToString();
+        nameText.text = player.name.ToString();
+        classText.text = textPlayerClass.ClassL.name;
     }
 }
