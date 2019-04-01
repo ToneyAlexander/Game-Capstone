943c1e24162c167bc3765cf9252f922731c0c9ee
Un-Jarvis <huang.2089@osu.edu>
2019-04-01 16:46:47 -0400

    Fixed spider death animation

--- Assets/Enemies/GenerateEnemies.cs
+++ Assets/Enemies/GenerateEnemies.cs
@@ -1,36 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GenerateEnemies : MonoBehaviour
 {
+    public EnemiesList enemiesTypes;
+
     private float spawnRange;
 
     private int enemiesNumber;
     private GameObject[] enemies;
 
-    public EnemiesList enemiesTypes;
+    // maxTimes is the max times that a navMeshAgent looks for a spawn position on navMesh, so that it won't lead to infinite loop
+    private int maxTimes;
+    private bool active;
 
     public void Start()
     {
-        spawnRange = 10f;
+        maxTimes = 50;
+
+        spawnRange = 20f;
         
         // Random number of enemies
-        // enemiesNumber = Random.Range(1, 3);
+        // enemiesNumber = Random.Range(5, 10);
         enemiesNumber = 1;
         enemies = new GameObject[enemiesNumber];
         
         for (int i = 0; i < enemiesNumber; i++)
         {
+            active = true;
             int type = Random.Range(0, enemiesTypes.enemeisList.Count - 1);
-            enemies[i] = Instantiate(enemiesTypes.enemeisList[type], transform.position, Quaternion.identity, transform);
+            enemies[i] = Instantiate(enemiesTypes.enemeisList[type], RandomPos(), Quaternion.identity, transform);
             enemies[i].name = "Enemy - " + enemiesTypes.enemeisList[type].name;
+            if (!active)
+            {
+                enemies[i].SetActive(false);
+                Debug.Log(enemies[i] + " is deactivated because its NavMeshAgent is not placed on a NavMesh.");
+            }
         }
     }
 
     private Vector3 RandomPos()
     {
-        Vector2 randomPoint = Random.insideUnitCircle * spawnRange;
-        return transform.position + new Vector3(randomPoint.x, 1.0f, randomPoint.y);
+        Vector2 randomPoint;
+        Vector3 randomPos;
+        NavMeshHit hit;
+        int i = 0;
+
+        do
+        {
+            randomPoint = Random.insideUnitCircle * spawnRange;
+            randomPos = transform.position + new Vector3(randomPoint.x, 0.0f, randomPoint.y);
+            i++;
+        }
+        while (!NavMesh.SamplePosition(randomPos, out hit, 5f, 1) && i < maxTimes);
+
+        if (i < maxTimes)
+        {
+            randomPos = hit.position;            
+        }
+        else
+        {
+            active = false;
+        }
+
+        return randomPos;
     }
 }
