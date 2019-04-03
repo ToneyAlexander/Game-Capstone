bf44b4b6bb0a83ba70977cc05b809f1d67d53b04
SquiddyStardust <thecanvashat@gmail.com>
2019-04-02 23:09:43 -0400

    boat added to scene

--- Assets/Scripts/TileGeneration/GenerateIsland.cs
+++ Assets/Scripts/TileGeneration/GenerateIsland.cs
@@ -86,6 +86,9 @@ public class GenerateIsland : MonoBehaviour
     [SerializeField]
     private ThemeDictionary themeDictionary;
 
+    [SerializeField]
+    private GameObject BackToHub;
+
 	private int themeCount;
 
 	[SerializeField]
@@ -479,7 +482,7 @@ public class GenerateIsland : MonoBehaviour
         //PAVAN
         if (true || this.Done())
         {
-            Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), this.GetBoatStart(), Quaternion.identity);
+            Instantiate(BackToHub, this.GetBoatStart(), Quaternion.identity);
             //WESLEY REMOVE THESE LINES
             remy.transform.position = this.PlayerStart;
             remy.GetComponent<RemyMovement>().setDetination(this.PlayerStart);
