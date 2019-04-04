2f229aeef1ff4c47cda931c11b8d6047d2a63349
SquiddyStardust <thecanvashat@gmail.com>
2019-04-04 00:11:09 -0400

    recruitment resets

--- Assets/GameManagement/GameStates/HubGameState.cs
+++ Assets/GameManagement/GameStates/HubGameState.cs
@@ -6,12 +6,18 @@ namespace CCC.GameManagement.GameStates
     /// Represents the state when the game is at the hub scene and waiting for 
     /// the player to select an island to go to.
     /// </summary>
+    /// 
+
     [CreateAssetMenu]
     public sealed class HubGameState : GameState
     {
+        [SerializeField]
+        private CrewController crewController;
+
         public override void Enter()
         {
             Debug.Log("In HubState.Enter");
+            crewController.recruited = false;
         }
 
         public override void Exit()
