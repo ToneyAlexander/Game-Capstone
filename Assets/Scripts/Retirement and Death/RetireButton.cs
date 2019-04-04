af3c0b2049207fcc7681472f6b71da52390161c5
ToneyAlexander <Alex.Toney11@gmail.com>
2019-04-04 05:11:59 -0400

    Cleaning up retirement

---
+++ Assets/Scripts/Retirement and Death/RetireButton.cs
@@ -0,0 +1,32 @@
+﻿using CCC.GameManagement;
+using CCC.GameManagement.GameStates;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class RetireButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager manager;
+
+    [SerializeField]
+    private GameState retire;
+
+    [SerializeField]
+    private SceneChanger changer;
+
+    [SerializeField]
+    private SceneReference retireScene;
+
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(RetireClicked);
+    }
+
+    void RetireClicked()
+    {
+        manager.TransitionTo(retire);
+    }
+}
