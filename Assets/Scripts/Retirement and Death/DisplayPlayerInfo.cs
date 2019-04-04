4b69ee2ce316cdfffdf70d40d8d3bc472e3e834e
ToneyAlexander <Alex.Toney11@gmail.com>
2019-04-04 13:37:47 -0400

    Death Fixing

--- Assets/Scripts/Retirement and Death/DisplayPlayerInfo.cs
+++ Assets/Scripts/Retirement and Death/DisplayPlayerInfo.cs
@@ -17,7 +17,7 @@ public class DisplayPlayerInfo : MonoBehaviour
     {
         this.GetComponent<Text>().text =
             blood.playerName + "\n" +
-            (blood.Age + 1) + " Cycles Old\n" +
+            (16 + blood.Age * 5) + " Cycles Old\n" +
             blood.currentClass.name + "\n" +
             "Level " + info.Level + "\n";
     }
