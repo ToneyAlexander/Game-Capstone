8a44c6321700c5fd3ee55a0683ffc007f883b5b6
Wesley Brown <nipponkid7@gmail.com>
2019-04-08 11:39:46 -0400

    Add Component to Allow GameObject to be Told to Stop Moving
    This will allow the creation of various commands that can be used to command
    a GameObject to stop moving.

---
+++ Assets/Movement/MovementStopper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace CCC.Movement
+{
+    /// <summary>
+    /// Represents a Component that allows its GameObject to be told to stop 
+    /// moving.
+    /// </summary>
+    [RequireComponent(typeof(RemyMovement))]
+    public sealed class MovementStopper : MonoBehaviour
+    {
+        /// <summary>
+        /// The RemyMovement Component that actually controls how the player 
+        /// moves.
+        /// </summary>
+        private RemyMovement remyMovement;
+
+        /// <summary>
+        /// Stop this MovementStopper's GameObject from moving.
+        /// </summary>
+        public void Stop()
+        {
+            // A RemyMovement Component can be told to stop moving by setting 
+            // its destination to its GameObject's current location.
+            remyMovement.setDetination(transform.position);
+        }
+
+        #region MonoBehaviour Messages
+        private void Awake()
+        {
+            remyMovement = GetComponent<RemyMovement>();
+        }
+        #endregion
+    }
+}
