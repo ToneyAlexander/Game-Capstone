b8b931264a4a5b209864dde90e136f6fbc9ef9e2
Wesley Brown <nipponkid7@gmail.com>
2019-04-08 14:00:03 -0400

    Create a Command to Stop Movement
    This will allow scripts to command a GameObject to stop without having to know
    how this is done internally.

---
+++ Assets/Movement/StopMovementCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace CCC.Movement
+{
+    /// <summary>
+    /// Represents a command that causes a GameObject to stop moving.
+    /// </summary>
+    public sealed class StopMovementCommand : ICommand
+    {
+        /// <summary>
+        /// Create a new StopMovementCommand that can stop a given 
+        /// MovementStopper.
+        /// </summary>
+        /// <returns>A new StopMovementCommand.</returns>
+        /// <param name="movementStopper">
+        /// The MovementStopper of the GameObject to stop movement of.
+        /// </param>
+        public static StopMovementCommand ForMovementStopper(
+            MovementStopper movementStopper)
+        {
+            return new StopMovementCommand(movementStopper);
+        }
+
+        /// <summary>
+        /// The MovementStopper that will stop its GameObject from moving.
+        /// </summary>
+        private readonly MovementStopper movementStopper;
+
+        private StopMovementCommand(MovementStopper movementStopper)
+        {
+            this.movementStopper = movementStopper;
+        }
+
+        #region ICommand
+        public void InvokeCommand()
+        {
+            movementStopper.Stop();
+        }
+        #endregion
+    }
+}
