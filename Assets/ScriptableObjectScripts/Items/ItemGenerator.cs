8897ad63973ef0c89bc661221c02e15702818193
Sergey Maltsev <maltsev.1@osu.edu>
2019-04-16 14:11:05 -0400

    Item rarities and affix odds added, affixes dont repeat now

--- Assets/ScriptableObjectScripts/Items/ItemGenerator.cs
+++ Assets/ScriptableObjectScripts/Items/ItemGenerator.cs
@@ -10,6 +10,7 @@ namespace CCC.Items
     public abstract class ItemGenerator : ScriptableObject
     {
         private static float[] tierOdds = {150, 110, 80, 55, 25, 10, 0};
+        private static float[] affixCountOdds = { 25, 55, 80, 55, 25, 10, 0 };
         private static readonly float tierIncrease = 1f;
 
         protected static void ApplyAffixes(List<AffixSetPrototype> allAffixSets, ItemGenerator gen, Item item, ItemPrototype proto)
@@ -17,14 +18,45 @@ namespace CCC.Items
             List<string> shortNames = new List<string>();
             List<string> longNames = new List<string>();
             float[] itemTierOdds = new float[tierOdds.Length];
+            float[] itemAffixCountOdds = new float[affixCountOdds.Length];
             System.Array.Copy(tierOdds, itemTierOdds, tierOdds.Length);
+            System.Array.Copy(affixCountOdds, itemAffixCountOdds, affixCountOdds.Length);
             for (int i = 1; i < itemTierOdds.Length; ++i)
             {
                 itemTierOdds[i] += tierIncrease * item.Tier * i;
             }
-            for (int i = 0; i < proto.MaxNumberAffixes; ++i)
+            float affixCountSum = 0;
+            for (int i = 1; i < affixCountOdds.Length; ++i)
             {
-                AffixSetPrototype toApply = allAffixSets[Random.Range(0, allAffixSets.Count)];
+                itemAffixCountOdds[i] += tierIncrease * item.Tier * i;
+                affixCountSum += itemAffixCountOdds[i];
+            }
+            int affixCount = 0;
+            float affixCountRng = Random.Range(0, affixCountSum);
+            float affixCountTry = 0;
+            for (int j = 0; j < itemAffixCountOdds.Length; ++j)
+            {
+                affixCountTry += itemAffixCountOdds[j];
+                if (affixCountRng <= affixCountTry)
+                {
+                    affixCount = j;
+                    break;
+                }
+            }
+            affixCount = proto.MaxNumberAffixes < affixCount ? proto.MaxNumberAffixes : affixCount;
+            List<int> usedAffixIndex = new List<int>();
+            for (int i = 0; i < affixCount; ++i)
+            {
+                AffixSetPrototype toApply;
+                int index;
+                int itr = 0;
+                do
+                {
+                    ++itr;
+                    index = Random.Range(0, allAffixSets.Count);
+                } while (usedAffixIndex.Contains(index) && itr < 100);
+                usedAffixIndex.Add(index);
+                toApply = allAffixSets[index];
                 float tierTry = 0;
                 float sum = 0;
                 for (int j = 0; j < toApply.Tiers.Count; ++j)
@@ -35,7 +67,7 @@ namespace CCC.Items
                 for (int j = 0; j < toApply.Tiers.Count; ++j)
                 {
                     tierTry += itemTierOdds[j];
-                    if(tier < tierTry)
+                    if(tier <= tierTry)
                     {
                         Debug.Log(tier + " actual affix: " + j + " item tier: " + item.Tier);
                         longNames.Add(item.Name + " of " + toApply.Tiers[j].affixName + " " + toApply.SetName);
